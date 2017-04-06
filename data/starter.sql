839409854586cf84dede2d79da60464a2a079d19
Hao(Leo) Luo <robzai@hotmail.com>
2017-04-05 17:02:45 -0700

    SQL starter changed

--- data/starter.sql
+++ data/starter.sql
@@ -22,39 +22,6 @@ SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `categories`
---
-
-DROP TABLE IF EXISTS `Categories`;
-CREATE TABLE `Categories` (
-  `id` varchar(1) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `description` text NOT NULL,
-  `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `Categories` (`id`, `name`, `description`, `image`) VALUES
-('d', 'Drinks', 'Purees made from the finest of Venusian insects, government-inspected.', 'catd.png'),
-('m', 'Mains', 'Made from only the finest ingredients found deep in the Martian jungle, and harvested or slaughtered by academy-trained druids, we bring you 45 day aged premium "meat".', 'catm.png'),
-('s', 'Sides', 'Perfect accompaniments to our mains, these side dish pairings have been exclsisvely formulated by Ben & Jerry.', 'cats.png');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ci_sessions`
---
-
-CREATE TABLE `ci_sessions` (
-  `id` varchar(128) NOT NULL,
-  `ip_address` varchar(45) NOT NULL,
-  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `data` blob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -89,65 +56,11 @@ INSERT INTO `Menu` (`id`, `name`, `description`, `price`, `picture`, `category`)
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orderitems`
---
-
-DROP TABLE IF EXISTS `Orderitems`;
-CREATE TABLE `Orderitems` (
-  `order` int(11) NOT NULL,
-  `item` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `special` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `Orders`;
-CREATE TABLE `Orders` (
-  `num` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `status` varchar(1) NOT NULL,
-  `total` decimal(10,2) NOT NULL,
-  `customer` varchar(64) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `Categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `ci_sessions`
---
-ALTER TABLE `ci_sessions`
-  ADD KEY `ci_sessions_timestamp` (`timestamp`);
-
---
 -- Indexes for table `menu`
 --
 ALTER TABLE `Menu`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `orderitems`
---
-ALTER TABLE `Orderitems`
-  ADD PRIMARY KEY (`order`,`item`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `Orders`
-  ADD PRIMARY KEY (`num`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
