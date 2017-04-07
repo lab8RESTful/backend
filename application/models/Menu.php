f4834831ff6ca593e322c00d8a6d4e1a0c6b3448
s-rebecca-fu <rebeccafu1991@gmail.com>
2017-04-06 22:48:55 -0400

    rules added

--- application/models/Menu.php
+++ application/models/Menu.php
@@ -8,17 +8,16 @@ class Menu extends MY_Model {
 		parent::__construct();
 	}
 
-	function getMenus() {
-	    $dishes = array();
-	    foreach ($this->all() as $dish) {
-	        $dish[] = array(
-	            "name" => $dish->name,
-                "description" => $dish-> description,
-                "price" => $dish->price,
-                "picture" => $dish->picture,
-                "category" => $dish->category
-            );
-        }
+    function rules() {
+        $config = [
+            ['field'=>'id', 'label'=>'Menu code', 'rules'=> 'required|integer'],
+            ['field'=>'name', 'label'=>'Item name', 'rules'=> 'required'],
+            ['field'=>'description', 'label'=>'Item description', 'rules'=> 'required|max_length[256]'],
+            ['field'=>'price', 'label'=>'Item price', 'rules'=> 'required|decimal'],
+            ['field'=>'picture', 'label'=>'Item picture', 'rules'=> 'required'],
+            ['field'=>'category', 'label'=>'Menu category', 'rules'=> 'required']
+        ];
+        return $config;
     }
 
 }
